ed73337dbd3175058443cff5d94142656ae6c9d1
MatijaPojatar <58627531+MatijaPojatar@users.noreply.github.com>
2025-01-15 15:16:55 +0100

    Change BOLD icon

--- src/icons.js
+++ src/icons.js
@@ -265,6 +265,7 @@ import _wsuperOETHb from '../icons/wsuperOETHb.svg';
 import _xSUSHI from '../icons/xSUSHI.svg';
 import _yCRV from '../icons/yCRV.svg';
 import _yveCRV from '../icons/yveCRV.svg';
+import _BOLD from '../icons/BOLD.svg';
 
 export default {
   '1INCH': _1INCH,
@@ -531,7 +532,8 @@ export default {
   'wsuperOETHb': _wsuperOETHb,
   'xSUSHI': _xSUSHI,
   'yCRV': _yCRV,
-  'yveCRV': _yveCRV
+  'yveCRV': _yveCRV,
+  'BOLD': _BOLD,
 };
 
 /** AUTOMATICALLY GENERATED FILE **/
